b6fd1279c6b9e9072e1e5a429fb18d31510bb3cc
Tuan Phan Le <coderbillzay@gmail.com>
2023-10-31 19:11:54 +0700

    change main

--- main.go
+++ main.go
@@ -1,32 +1,26 @@
 package main
 
-import (
-	"os"
-
-	"github.com/billzayy/jenkins-golang/services"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-)
+import "fmt"
 
 func main() {
-	godotenv.Load()
+	// godotenv.Load()
 
-	app := gin.Default()
+	// app := gin.Default()
 
-	app.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"*"},
-		ExposeHeaders:    []string{"Link"},
-		AllowAllOrigins:  true,
-		AllowCredentials: false,
-		MaxAge:           300,
-	}))
+	// app.Use(cors.New(cors.Config{
+	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	// 	AllowHeaders:     []string{"*"},
+	// 	ExposeHeaders:    []string{"Link"},
+	// 	AllowAllOrigins:  true,
+	// 	AllowCredentials: false,
+	// 	MaxAge:           300,
+	// }))
 
-	api := app.Group("/api")
-	{
-		api.GET("/get-user", services.GetUser)
-	}
+	// api := app.Group("/api")
+	// {
+	// 	api.GET("/get-user", services.GetUser)
+	// }
 
-	app.Run(":" + os.Getenv("PORT"))
+	// app.Run(":" + os.Getenv("PORT"))
+	fmt.Println("Hello")
 }
