094263a8535d65fdc4f350f4ca0701daa65fcc50
Tuan Phan Le <coderbillzay@gmail.com>
2023-11-01 18:21:30 +0700

    delete into main

--- main.go
+++ main.go
@@ -1,33 +1,7 @@
 package main
 
-import (
-	"os"
-
-	"github.com/billzayy/jenkins-golang/services"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-)
+import "fmt"
 
 func main() {
-	godotenv.Load()
-
-	app := gin.Default()
-
-	app.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"*"},
-		ExposeHeaders:    []string{"Link"},
-		AllowAllOrigins:  true,
-		AllowCredentials: false,
-		MaxAge:           300,
-	}))
-
-	api := app.Group("/api")
-	{
-		api.GET("/get-user", services.GetUserServices)
-	}
-
-	app.Run(":" + os.Getenv("PORT"))
-	// fmt.Println("Hello Jenkins Integrate with Github")
+	fmt.Println("Hello Jenkins Integrate with Github")
 }
--- services/GetUser.go
+++ services/GetUser.go
@@ -1,11 +1,1 @@
 package services
-
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-func GetUserServices(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"status": "Successful", "data": "Nothing"})
-}
