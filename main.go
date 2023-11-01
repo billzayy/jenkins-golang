bfc4e5349139b5ef63f119f9b4f001ccac635ce8
Tuan Phan Le <coderbillzay@gmail.com>
2023-11-01 13:46:56 +0700

    change all files

--- main.go
+++ main.go
@@ -3,24 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	// godotenv.Load()
-
-	// app := gin.Default()
-
-	// app.Use(cors.New(cors.Config{
-	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	// 	AllowHeaders:     []string{"*"},
-	// 	ExposeHeaders:    []string{"Link"},
-	// 	AllowAllOrigins:  true,
-	// 	AllowCredentials: false,
-	// 	MaxAge:           300,
-	// }))
-
-	// api := app.Group("/api")
-	// {
-	// 	api.GET("/get-user", services.GetUser)
-	// }
-
-	// app.Run(":" + os.Getenv("PORT"))
 	fmt.Println("Hello Jenkins Integrate with Github")
 }
